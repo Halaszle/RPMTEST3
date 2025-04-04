16ede3d06c87654f8aae1dbce769faac00fce09f
Halszle <konhalaszle@gmail.com>
2025-04-04 05:20:20 +0200

    Updated homepage

--- app/music/layout.js
+++ app/music/layout.js
@@ -4,26 +4,19 @@ export const metadata = {
   keywords: 'Red Pine Mushroom, post-rock, rock psychodeliczny, desert rock, polski zespół, muzyka instrumentalna',
   robots: 'index, follow', // Upewnij się, że Google może indeksować
   openGraph: {
-    title: 'BIO - Red Pine Mushroom | Post-Rock Band',
+    title: 'Music - Red Pine Mushroom | Post-Rock Band',
     description: 'Poznaj historię zespołu Red Pine Mushroom – polskiego tria łączącego post-rock, rock psychodeliczny i desert rock.',
-    url: 'https://twojadomena.com/bio',
+    url: 'https://redpinemushroom.com/music',
     siteName: 'Red Pine Mushroom',
     type: 'website',
     images: [
       {
-        url: 'https://twojadomena.com/images/bio-cover.jpg', // Upewnij się, że ten obraz istnieje
-        width: 1200,
-        height: 630,
-        alt: 'Red Pine Mushroom - zespół post-rockowy',
+        url: 'https://redpinemushroom.com/logomain.png', // Upewnij się, że ten obraz istnieje
+        width: 1184,
+        height: 1184,
+        alt: 'Red Pine Mushroom - post-rock, desert rock, psychedelic band',
       },
     ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    site: '@TwojTwitter',
-    title: 'BIO - Red Pine Mushroom | Post-Rock Band',
-    description: 'Poznaj historię zespołu Red Pine Mushroom – polskiego tria łączącego post-rock, rock psychodeliczny i desert rock.',
-    images: ['https://twojadomena.com/images/bio-cover.jpg'],
   },
 };
 
