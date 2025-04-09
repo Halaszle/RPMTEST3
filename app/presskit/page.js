c774fc3be0d02751762fe47f59e747b9d7bd5d60
Halszle <konhalaszle@gmail.com>
2025-04-09 12:26:39 +0200

    Updated homepage

--- app/presskit/page.js
+++ app/presskit/page.js
@@ -50,7 +50,7 @@ export default function Bio() {
 			<div className={styles.mediaresources}>
 				<h2>PRESS PACK</h2>
 					<p>Materials, photos, bios and press releases</p>
-						<a href="google.com" className={styles.btnlarge}>Complete Press Pack (zip) →</a>
+						<a href="https://drive.google.com/drive/folders/1ID5AlEBFTkSIsvJIKcwBi2cieGxV3wiz?usp=drive_link" className={styles.btnlarge}>Complete Press Pack (zip) →</a>
 							<br></br><br></br>
 				<h3>MEDIA RESOURCES</h3>	
 
@@ -58,41 +58,41 @@ export default function Bio() {
 				<div className={styles.ResourceSection}>
 					<span>INFO + BIO</span>
 						<div className={styles.ButtonGroup}>
-							<a href="google.com" className={styles.btn}>Info & Bio (pdf) →</a>						
+							<a href="https://drive.google.com/drive/folders/1ID5AlEBFTkSIsvJIKcwBi2cieGxV3wiz?usp=drive_link" className={styles.btn}>Info & Bio (pdf) →</a>						
 						</div>				
 				</div>
 <br></br>
 				<div className={styles.ResourceSection}>
 					<span>LOGO</span>
 						<div className={styles.ButtonGroup}>
-							<a href="google.com" className={styles.btn}>Logo (png) →</a>		
-							<a href="google.com" className={styles.btn}>Logo (svg) →</a>	
-							<a href="google.com" className={styles.btn}>Logo (pdf) →</a>								
+							<a href="https://drive.google.com/drive/folders/18Nuz46UN0yRscNfz8smDdKwMzNANplJK?usp=drive_link" className={styles.btn}>Logo (png) →</a>		
+							<a href="https://drive.google.com/drive/folders/18Nuz46UN0yRscNfz8smDdKwMzNANplJK?usp=drive_link" className={styles.btn}>Logo (svg) →</a>	
+							<a href="https://drive.google.com/drive/folders/18Nuz46UN0yRscNfz8smDdKwMzNANplJK?usp=drive_link" className={styles.btn}>Logo (pdf) →</a>								
 						</div>				
 				</div>
 <br></br>
 				<div className={styles.ResourceSection}>
 					<span>PHOTOS</span>
 						<div className={styles.ButtonGroup}>
-							<a href="google.com" className={styles.btn}>Band (zip) →</a>						
+							<a href="https://drive.google.com/drive/folders/1iy5zai1PuzJAPU9yBMxzCJEdjL7VYWvM?usp=drive_link" className={styles.btn}>Band (zip) →</a>						
 						</div>				
 				</div>
 <br></br>
 				<div className={styles.ResourceSection}>
 					<span>ALBUM COVERS</span>
 						<div className={styles.ButtonGroup}>
-							<a href="google.com" className={styles.btn}>Album covers (zip) →</a>						
+							<a href="https://drive.google.com/drive/folders/14KuU57FvF3hn5d9jWSx4CPO9iInOUdnH?usp=drive_link" className={styles.btn}>Album covers (zip) →</a>						
 						</div>				
 				</div>
 <br></br>
 				<div className={styles.ResourceSection}>
 					<span>LINKS</span>
 						<div className={styles.ButtonGroup}>
-							<a href="google.com" className={styles.btn}>Facebook →</a>		
-							<a href="google.com" className={styles.btn}>Instagram →</a>	
-							<a href="google.com" className={styles.btn}>YouTube →</a>		
-							<a href="google.com" className={styles.btn}>Bandcamp →</a>	
-							<a href="google.com" className={styles.btn}>Spotify →</a>									
+							<a href="https://www.facebook.com/redpinemushroom" className={styles.btn}>Facebook →</a>		
+							<a href="https://instagram.com/redpinemushroomofficial" className={styles.btn}>Instagram →</a>	
+							<a href="https://www.youtube.com/@redpinemushroom" className={styles.btn}>YouTube →</a>		
+							<a href="https://redpinemushroom.bandcamp.com/" className={styles.btn}>Bandcamp →</a>	
+							<a href="https://open.spotify.com/artist/1xwwutIpfyNvuVyWYXw2Gr?si=odDG0CRnQgC0MWwD9RCu5A" className={styles.btn}>Spotify →</a>									
 						</div>				
 				</div>
 <br></br>
